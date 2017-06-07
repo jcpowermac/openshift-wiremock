f459e808d64e65855dd13831e6ed42ae17244d75
Joseph Callen <jcallen@redhat.com>
2017-06-07 17:34:19 -0400

    http command line changes

--- init.sh
+++ init.sh
@@ -2,6 +2,8 @@
 
 set -x
 
+#PROJECT_ID="p17633880910e488f5949aab3ad76cd4317542a7a06"
+#DIGEST="sha256:144c7752a692dbcfd32339a9c9acd1e56d03de9e1aa617385a630824a4518456"
 
 java -jar /opt/wiremock/wiremock.jar \
      --root-dir /var/lib/wiremock \
@@ -12,9 +14,9 @@ java -jar /opt/wiremock/wiremock.jar \
 
 sleep 5
 
-http --pretty=all --traceback --json POST :8080/api/container/status pid=\"${PROJECT_ID}\" secret=\"${SECRET}\"
-http --pretty=all --traceback --json POST :8080/api/container/publish pid=\"${PROJECT_ID}\" secret=\"${SECRET}\" docker_image_digest=\"${DIGEST}\"
-http --pretty=all --traceback --json POST :8080/api/container/scanResults pid=\"${PROJECT_ID}\" secret=\"${SECRET}\" docker_image_digest=\"${DIGEST}\"
+http --pretty=all --traceback --json POST :8080/api/container/status -- pid=${PROJECT_ID} secret=${SECRET}
+http --pretty=all --traceback --json POST :8080/api/container/publish -- pid=${PROJECT_ID} secret=${SECRET} docker_image_digest=${DIGEST}
+http --pretty=all --traceback --json POST :8080/api/container/scanResults -- pid=${PROJECT_ID} secret=${SECRET} docker_image_digest=${DIGEST}
 
 sleep 5
 http --pretty=all --traceback --json POST :8080/__admin/shutdown
