11923a593250a9ccb09738300113eba747ce1820
Joseph Callen <jcallen@redhat.com>
2017-06-07 16:06:02 -0400

    adding files

--- Dockerfile
+++ Dockerfile
@@ -1,12 +1,15 @@
 FROM centos
 
-RUN yum install -y java-1.8.0-openjdk && \
+RUN yum install -y epel-release java-1.8.0-openjdk && \
+    yum install python2-httpie && \
     yum clean all && \
     mkdir -p /opt/wiremock/ && \
     mkdir -p /var/lib/wiremock && \
     chown -R 10001:0 /opt/wiremock /var/lib/wiremock && \
     chmod -R 777 /opt/wiremock /var/lib/wiremock && \
     curl -o /opt/wiremock/wiremock.jar http://repo1.maven.org/maven2/com/github/tomakehurst/wiremock-standalone/2.6.0/wiremock-standalone-2.6.0.jar
+
+COPY init.sh /opt/wiremock
 
 USER 10001
 EXPOSE 8080
---
+++ init.sh
@@ -0,0 +1,20 @@
+#!/bin/sh
+
+set -x
+
+
+java -jar /opt/wiremock/wiremock.jar \
+     --root-dir /var/lib/wiremock \
+     --proxy-all="https://connect.redhat.com" \
+     --bind-address="0.0.0.0" \
+     --record-mappins \
+     --verbose &
+
+sleep 10
+
+http --pretty=all --traceback --json POST :8080/api/container/status pid=${PROJECT_ID} secret=${SECRET}
+http --pretty=all --traceback --json POST :8080/api/container/publish pid=${PROJECT_ID} secret=${SECRET} docker_image_digest=${DIGEST}
+http --pretty=all --traceback --json POST :8080/api/container/scanResults pid=${PROJECT_ID} secret=${SECRET} docker_image_digest=${DIGEST}
+
+sleep 15
+http --pretty=all --traceback --json POST :8080/__admin/shutdown
